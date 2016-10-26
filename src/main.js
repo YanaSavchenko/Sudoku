b585ce6154c75ee03691db334cf1b0550144cb0e
Yana Savchenko <yana.savchenko@amsurg.com>
2016-10-26 10:58:14 -0500

    App skeleton

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import 'babel-polyfill';
+
+import $        from 'jquery';
+import ReactDOM from 'react-dom';
+import React    from 'react';
+
+window.React = React;
+window.$     = window.jQuery = $;
+
+import { Router, hashHistory } from 'react-router';
+import routes   from './routes.jsx';
+
+import thunk                             from 'redux-thunk';
+import { Provider }                      from 'react-redux';
+import { createStore, applyMiddleware  } from 'redux';
+// import reducer                           from './reducers';
+
+$(document).ready(function() {
+    // const store = createStore(
+    //     reducer,
+    //     applyMiddleware(thunk)
+    // );
+
+    ReactDOM.render(
+        // <Provider store={store}>
+            <Router
+                createElement = {(Component, props) => <Component {...props} />}
+                routes        = {routes}
+                history       = {hashHistory} />,
+        // </Provider>,
+        document.getElementById('content')
+    );
+});
