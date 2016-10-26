4dab65de0477a31ee514afc119a7684045e450c1
Yana Savchenko <yana.savchenko@amsurg.com>
2016-10-26 10:57:46 -0500

    Add StartPage container

---
+++ src/containers/StartPageContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import StartPage from '../components/pages/StartPage.jsx';
+
+export default class StartPageContainer extends React.Component {
+    render() {
+        return (
+            <div className='StartPageContainer'>
+                <StartPage />
+            </div>
+        );
+    }
+}
