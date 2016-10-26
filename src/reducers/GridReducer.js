05d44081726140fa9281e0d08b7f8afc0aaa7f5b
Yana Savchenko <yana.savchenko@amsurg.com>
2016-10-26 14:52:21 -0500

    Add clear method

--- src/reducers/GridReducer.js
+++ src/reducers/GridReducer.js
@@ -1,34 +1,43 @@
 import ec from '../eventConstants';
 import _  from 'lodash';
 
+const initialGrid = [
+    [8, 0, 0, 4, 0, 6, 0, 0, 7],
+    [0, 0, 0, 0, 0, 0, 4, 0, 0],
+    [0, 1, 0, 0, 0, 0, 6, 5, 0],
+    [5, 0, 9, 0, 3, 0, 7, 8, 0],
+    [0, 0, 0, 0, 7, 0, 0, 0, 0],
+    [0, 4, 8, 0, 2, 0, 1, 0, 3],
+    [0, 5, 2, 0, 0, 0, 0, 9, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [3, 0, 0, 9, 0, 2, 0, 0, 5]
+];
+
 const initialState = {
-    grid: [
-        [8, 0, 0, 4, 0, 6, 0, 0, 7],
-        [0, 0, 0, 0, 0, 0, 4, 0, 0],
-        [0, 1, 0, 0, 0, 0, 6, 5, 0],
-        [5, 0, 9, 0, 3, 0, 7, 8, 0],
-        [0, 0, 0, 0, 7, 0, 0, 0, 0],
-        [0, 4, 8, 0, 2, 0, 1, 0, 3],
-        [0, 5, 2, 0, 0, 0, 0, 9, 0],
-        [0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [3, 0, 0, 9, 0, 2, 0, 0, 5]
-    ],
+    grid: JSON.parse(JSON.stringify(initialGrid)),
 
     status: {
         isSolved: false,
         isEdited: false
     },
 
-    history: []
+    history: [{
+        grid: JSON.parse(JSON.stringify(initialGrid)),
+        status: {
+            isSolved: false,
+            isEdited: false
+        },
+        history: []
+    }]
 };
 
 /*eslint-disable */
 export default function grid( state = initialState, action) {
     switch (action.type) {
         case ec.GRID_INPUT_VALUE: return inputValue(state, action.data);
-        case ec.GRID_SOLVE:       return solve(state);
-        case ec.GRID_UNDO:        return undo(state);
         case ec.GRID_CLEAR:       return clear(state);
+        case ec.GRID_UNDO:        return undo(state);
+        case ec.GRID_SOLVE:       return solve(state);
 
         default: return state;
     }
@@ -41,6 +50,17 @@ function inputValue(state, data) {
     updatedState.grid[data.row][data.col] = data.value;
     updatedState.status.isEdited = true;
     updatedState.history.push(updatedState);
+
+    return updatedState;
+}
+
+function clear(state) {
+    const firstState   = _.cloneDeep(state.history[0]);
+    const updatedState = {};
+
+    updatedState.grid    = firstState.grid;
+    updatedState.status  = firstState.status;
+    updatedState.history = [state.history[0]];
 
     return updatedState;
 }
