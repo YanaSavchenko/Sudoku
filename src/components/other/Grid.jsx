1d9c540bf1bbb8778455512bde3f98dc57897a37
Yana Savchenko <yana.savchenko@amsurg.com>
2016-10-26 14:17:10 -0500

    Pass method

--- src/components/other/Grid.jsx
+++ src/components/other/Grid.jsx
@@ -26,17 +26,7 @@ export default class Grid extends React.Component {
     }
 
     render() {
-        const grid = [
-            [8, 0, 0, 4, 0, 6, 0, 0, 7],
-            [0, 0, 0, 0, 0, 0, 4, 0, 0],
-            [0, 1, 0, 0, 0, 0, 6, 5, 0],
-            [5, 0, 9, 0, 3, 0, 7, 8, 0],
-            [0, 0, 0, 0, 7, 0, 0, 0, 0],
-            [0, 4, 8, 0, 2, 0, 1, 0, 3],
-            [0, 5, 2, 0, 0, 0, 0, 9, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0, 0],
-            [3, 0, 0, 9, 0, 2, 0, 0, 5]
-        ];
+        const grid = this.props.grid || [];
 
         return (
             <div className='Grid'>
