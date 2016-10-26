a22e36fd1dfc1135b7c1078b90b9c95822371a55
Yana Savchenko <yana.savchenko@amsurg.com>
2016-10-26 13:22:56 -0500

    Redirect to game page on button click

--- src/containers/StartPageContainer.jsx
+++ src/containers/StartPageContainer.jsx
@@ -3,11 +3,20 @@ import React from 'react';
 import StartPage from '../components/pages/StartPage.jsx';
 
 export default class StartPageContainer extends React.Component {
+    start() {
+        this.context.router.push('/game');
+    }
+
     render() {
         return (
             <div className='StartPageContainer'>
-                <StartPage />
+                <StartPage
+                    onStart = {this.start.bind(this)} />
             </div>
         );
     }
 }
+
+StartPageContainer.contextTypes = {
+    router: React.PropTypes.object
+};
