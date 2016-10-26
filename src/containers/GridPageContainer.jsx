df72be3bf8c7a40dfe2905005ff4dc8126059d10
Yana Savchenko <yana.savchenko@amsurg.com>
2016-10-26 13:24:10 -0500

    Add GridPage controller

---
+++ src/containers/GridPageContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GridPage from '../components/pages/GridPage.jsx';
+
+export default class GridPageContainer extends React.Component {
+    render() {
+        return (
+            <div className='GridPageContainer'>
+                <GridPage />
+            </div>
+        );
+    }
+}
