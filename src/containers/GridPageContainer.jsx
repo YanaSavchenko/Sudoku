b84968067976f1cafa6eb2081bf99cf285dbae8f
Yana Savchenko <yana.savchenko@amsurg.com>
2016-10-26 14:18:20 -0500

    Use actions and store

--- src/containers/GridPageContainer.jsx
+++ src/containers/GridPageContainer.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 
+import actions     from '../actions';
+import { connect } from 'react-redux';
+
 import GridPage from '../components/pages/GridPage.jsx';
 
-export default class GridPageContainer extends React.Component {
+class GridPageContainer extends React.Component {
     render() {
+        const grid = this.props.grid;
+
         return (
             <div className='GridPageContainer'>
-                <GridPage />
+                <GridPage
+                    grid         = {grid.array}
+                    onInputValue = {this.props.inputValue.bind(this)} />
             </div>
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        grid: {
+            array:   state.grid,
+            history: state.history,
+            status:  state.status
+        }
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        inputValue: (row, col, value) => {
+            dispatch(actions.grid.inputValue(row, col, value));
+        }
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GridPageContainer);
