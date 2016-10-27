834eb8c66ef326927f7c84175ec3175ffa8fc975
Yana Savchenko <yana.savchenko@amsurg.com>
2016-10-27 16:36:12 -0500

    Add GRID_CHECK

--- src/eventConstants.js
+++ src/eventConstants.js
@@ -2,5 +2,6 @@ module.exports = {
     GRID_INPUT_VALUE: 'GRID_INPUT_VALUE',
     GRID_SOLVE:       'GRID_SOLVE',
     GRID_UNDO:        'GRID_UNDO',
-    GRID_CLEAR:       'GRID_CLEAR'
+    GRID_CLEAR:       'GRID_CLEAR',
+    GRID_CHECK:       'GRID_CHECK'
 };
